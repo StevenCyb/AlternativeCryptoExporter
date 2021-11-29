a365482a3bfdbf874bc061345b1f59fe2d0e2667
Steven Cybinski <41087154+StevenCyb@users.noreply.github.com>
2021-11-29 20:14:19 +0100

    add ticker logic and populate metrics

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,6 @@
 
 ## [0.1.0]
 ### Added
+- add ticker logic and populate metrics
 - ticker routine and supported currency check
 - configuration through arguments
--- go/config.go
+++ go/config.go
@@ -8,7 +8,7 @@ import (
 var (
 	logLevel          int
 	listen            string
-	convertCurrency   string
+	quotes            string
 	currencyWatchlist model.StringArrayFlags
 )
 
@@ -16,7 +16,7 @@ func init() {
 	flag.IntVar(&logLevel, "log-level", 4, "Log level {'0' NONE, '1' FATAL, '2' ERROR, '3' WARNING, '4' INFO, '5' DEBUG}")
 	flag.StringVar(&listen, "listen", ":8080", "Set listen endpoint e.g ':8080'")
 
-	flag.StringVar(&convertCurrency, "convert-currency", "", "Currency to display crypto value e.g. {USD', 'EUR', 'GBP', 'RUB', 'JPY', 'CAD', 'KRW', 'PLN', 'BTC', 'ETH', 'XRP', 'LTC}")
+	flag.StringVar(&quotes, "quotes", "USD", "Currency to display crypto quotes e.g. {USD', 'EUR', 'GBP', 'RUB', 'JPY', 'CAD', 'KRW', 'PLN', 'BTC', 'ETH', 'XRP', 'LTC}")
 	flag.Var(&currencyWatchlist, "watch", "Define currency to watch e.g. {'BTC', 'LTC'}")
 
 	flag.Parse()
